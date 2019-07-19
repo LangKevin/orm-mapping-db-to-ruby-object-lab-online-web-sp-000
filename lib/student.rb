2294f083920033a5370bfb9db7430c900afdd954
kevin lang <langkevina@gmail.com>
2019-07-19 01:22:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,10 +1,11 @@
 class Student
     attr_accessor :name, :grade
     attr_reader :id
-    def initialize(name, grade, id = nil)
-      @name = name
-      @grade = grade
-      @id = id
+    # def initialize(name, grade, id = nil)
+    def initialize
+      # @name = name
+      # @grade = grade
+      # @id = id
     end
     def self.create_table
       sql = <<-SQL
