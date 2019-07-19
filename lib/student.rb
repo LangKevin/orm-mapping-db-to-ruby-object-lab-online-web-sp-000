76c7d601702b6922e7dd6365ce10761294ed1b30
kevin lang <langkevina@gmail.com>
2019-07-19 01:16:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,7 +35,7 @@ class Student
       student
     end
     def self.new_from_db(row)
-      new_student = self.new(row[1], row[2])
+      new_student = Student.new(row[1], row[2])
       # new_student.id = row[0]
       # new_student.name = row[1]
       # new_student.grade = row[2]
