c6e3581d65001b54fd613069900c7a4701f19ab7
kevin lang <langkevina@gmail.com>
2019-07-19 01:07:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,14 +52,14 @@ class Student
         self.new_from_db(row)
       end.first
     end
-    # def self.all
-    #   sql = <<-SQL
-    #     SELECT * FROM students
-    #   SQL
-    #   DB[:conn].execute(sql).map do |row|
-    #     self.new_from_db(row)
-    #   end
-    # end
+    def self.all
+      sql = <<-SQL
+        SELECT * FROM students
+      SQL
+      DB[:conn].execute(sql).map do |row|
+        self.new_from_db(row)
+      end
+    end
     # def self.all_students_in_grade_9
     #   sql = <<-SQL
     #    SELECT *
